516096221cf8dfe2478ce2d64ac0ea6d1a1dccb5
kalpainnovativesolution <160105939+kalpainnovativesolution@users.noreply.github.com>
2025-09-15 16:25:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def process_excel_file(filepath: str) -> dict:
     for i, val in enumerate(combined_df['Positive %']):
         ax_combined.text(
             i + 0.25,
-            combined_df['Positive Pregnancies'].iloc[i] + 10,  # offset to avoid overlap
+            combined_df['Positive Pregnancies'].iloc[i] + 20,  # offset to avoid overlap
             f"{val}%",
             ha='center', va='bottom', fontsize=9, color='darkgreen', fontweight='bold'
         )
@@ -258,4 +258,5 @@ if uploaded_file is not None:
         st.subheader("📄 Pregnancy Report by Organization")
         st.dataframe(result["tables"]["Pregnancy Report by Organization"])
         st.pyplot(result["org_graph"])
+
 
